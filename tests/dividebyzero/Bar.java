927709c7953a47c74ffaabc8083a57199d07e6fd
thoriumrobot <74688646+thoriumrobot@users.noreply.github.com>
2022-09-28 19:08:57 -0400

    Add files via upload

---
+++ tests/dividebyzero/Bar.java
@@ -0,0 +1,12 @@
+import org.checkerframework.checker.dividebyzero.qual.*;
+
+class Bar {
+
+    public static void f() {
+        int one  = 1;
+        int zero = 0;
+	// :: error: divide.by.zero
+	int x = one % zero;
+    }
+
+}
