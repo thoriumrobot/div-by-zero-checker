376335cf1d4bafc67e1168cd6f068f3ad0bac4e8
thoriumrobot <74688646+thoriumrobot@users.noreply.github.com>
2022-09-29 16:52:14 -0400

    Add files via upload

--- tests/dividebyzero/Bar.java
+++ tests/dividebyzero/Bar.java
@@ -5,11 +5,16 @@ class Bar {
     public static void f() {
         int one  = 1;
         int zero = 0;
-	// :: error: divide.by.zero
-	int x = one % zero;
-	int y = one;
-	// :: error: divide.by.zero
-	y %= zero;
+        
+	      // :: error: divide.by.zero
+	      int x = one % zero;
+	      int y = one;
+	      // :: error: divide.by.zero
+	      y %= zero;
+         for(int i=10; i>=0; i--) {
+           // :: error: divide.by.zero
+           int z=1/i;
+         }
     }
 
 }
